eae0d89ebd22d3d869c56a8484100d016cb49e71
Yannn18 <azaryayanuar18@gmail.com>
2026-02-26 10:09:05 +0700

    only desktop console

--- src/strukturdata/csimpul.java
+++ src/strukturdata/csimpul.java
@@ -4,11 +4,12 @@ import data.cNota;
 import java.time.LocalDate;
 
 public class csimpul {
+
     public String idMember; // Bisa diisi nama pembeli atau ID member
     public cNota nota;
     public int status; // 0 = belum diproses, 1 = sudah diproses
     public csimpul next, prev;
-     public LocalDate tanggalTransaksi;
+    public LocalDate tanggalTransaksi;
 
     public csimpul(String id, cNota n) {
         idMember = id;
